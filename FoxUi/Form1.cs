b55655f35f6a6c727f00bdd7b6662f070c3b89c6
cocomarinaru <g1tcoco>
2017-09-19 15:44:26 +0300

    tested fox connector
    tested fox connector

--- FoxUi/Form1.cs
+++ FoxUi/Form1.cs
@@ -19,7 +19,26 @@ namespace FoxUi
 
         private void exportButton_Click(object sender, EventArgs e)
         {
+            System.Diagnostics.Debug.WriteLine("Click on export");
             
+            DateTime starDate = startDatePicker.Value;
+            DateTime stopDate = stopDatePicker.Value;
+
+            System.Diagnostics.Debug.WriteLine(starDate);
+            System.Diagnostics.Debug.WriteLine(stopDate);
+            
+            FoxDBConector foxDbConector = new FoxDBConector();
+            
+            foxDbConector.connect();
+
+            DataTable testData = foxDbConector.GetYourData();
+
+            int count = testData.Rows.Count;
+
+            System.Diagnostics.Debug.WriteLine("Found " + count);
+
+
+
         }
     }
 }
---
+++ FoxUi/FoxDBConector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+
+namespace FoxUi
+{
+    public class FoxDBConector
+    {
+        public void connect()
+        {
+            Console.Write("Coco connect");
+        }
+    
+        public DataTable GetYourData()
+        {
+            DataTable YourResultSet = new DataTable();
+
+            OleDbConnection yourConnectionHandler = new OleDbConnection(
+                @"Provider=VFPOLEDB.1;Data Source=E:\work\projects\spyfox\db\");
+
+            // Open the connection, and if open successfully, you can try to query it
+            yourConnectionHandler.Open();
+
+            if (yourConnectionHandler.State == ConnectionState.Open)
+            {
+                string mySQL = "select * from totcom";  // dbf table name
+
+                OleDbCommand MyQuery = new OleDbCommand(mySQL, yourConnectionHandler);
+                OleDbDataAdapter DA = new OleDbDataAdapter(MyQuery);
+
+                DA.Fill(YourResultSet);
+
+                yourConnectionHandler.Close();
+            }
+
+            return YourResultSet;
+        }
+    }
+}
